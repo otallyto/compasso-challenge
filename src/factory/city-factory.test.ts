8601d55dcdc02990778aadc76209d7250eb79586
otallyto <rodrigues.tallyto@hotmail.com>
2021-10-11 21:21:38 -0300

    chore: remove factory test

--- src/factory/city-factory.test.ts
+++
@@ -1,8 +0,0 @@
-import { CityController } from '@controller/city-controller'
-import { cityFactory } from './city-factory'
-
-describe('City Factory', () => {
-  it('Deve retornar uma instancia de CityController', () => {
-    expect(cityFactory()).toBeInstanceOf(CityController)
-  })
-})
--- src/factory/client-factory.test.ts
+++
@@ -1,8 +0,0 @@
-import { ClientController } from '@controller/client-controller'
-import { clientFactory } from './client-factory'
-
-describe('Client Factory', () => {
-  it('Deve retornar uma instancia de ClientController', () => {
-    expect(clientFactory()).toBeInstanceOf(ClientController)
-  })
-})
